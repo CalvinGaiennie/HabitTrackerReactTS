c281847ec061b1e715c81fe73ab67ea37f17de6b
CalvinGaiennie <calvingaiennie@gmail.com>
2025-09-20 23:21:27 -0500

    comment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,14 @@ import type { MetricCreate } from "../types/Metrics.ts";
 // import { createMetric } from "../services/metrics.ts";
 
 function HomePage() {
-  const form: MetricCreate = {
-    user_id: 1,
-    name: "test metric",
-    is_required: false,
-    notes_on: false,
-    data_type: "text",
-    active: true,
-  };
+  // const form: MetricCreate = {
+  //   user_id: 1,
+  //   name: "test metric",
+  //   is_required: false,
+  //   notes_on: false,
+  //   data_type: "text",
+  //   active: true,
+  // };
 
   // useEffect(() => {
   //   createMetric(form)
