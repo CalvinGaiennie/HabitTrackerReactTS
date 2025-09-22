fb575b5377b9542781072045d15079ca55098b63
CalvinGaiennie <calvingaiennie@gmail.com>
2025-09-21 23:41:26 -0500

    updated my fakeusersettings

--- src/fakeUserSettings.ts
+++ src/fakeUserSettings.ts
@@ -2,59 +2,11 @@ export const fakeUserSettings = {
   homePageLayout: [
     {
       section: "To Log",
-      metricIds: [1, 3],
+      metricIds: [1, 2, 3, 4, 9],
     },
     {
       section: "To Do",
-      metricIds: [],
+      metricIds: [5, 6, 7, 8, 10],
     },
   ],
 };
-
-
-/*
-TO LOG
-BW
-Protein Intake
-Hours in bed last night
-congestion
-neck pain
-daily energy
-Net Liquid Assets 
-
-TO DO
-15 mins workout y/n
-10 mins cleaning y/n
-10 mins straight reading y/n
-Lift >30mins  y/n
-hours worked today
-BW + 10 G of protein Y/n 
-
-
-
-// Standard Metrics
-BW
-Protein Intake
-Hours in bed last night
-15 mins workout y/n
-10 mins cleaning y/n
-10 mins straight reading y/n
-congestion
-neck pain
-daily energy
-Net Liquid Assets 
-
-
-//work towards current goals
-get checking account to $3000 and keep there or higher forever
-Lift >30mins  y/n
-hours worked today 
-
-
-//current goals
-Bring GA on a trip
-gain 5 pounds
-save $3000
-average more than 32 hours a week
-BEJJ >1 a week
-*/
