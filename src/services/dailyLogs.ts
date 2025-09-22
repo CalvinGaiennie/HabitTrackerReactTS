f974c7c65566c149e914852a733d9c2585849700
CalvinGaiennie <calvingaiennie@gmail.com>
2025-09-22 00:10:18 -0500

    small fix

--- src/services/dailyLogs.ts
+++ src/services/dailyLogs.ts
@@ -4,14 +4,12 @@ import type { DailyLog } from "../types/dailyLogs.ts"
 
 
 export async function saveDailyLog(log: Omit<DailyLog, "id" | "created_at">) {
-    return request<DailyLog>("/daily_logs", {
+    return request<DailyLog>("/daily_logs/", {
         method: "POST",
         body: JSON.stringify(log),
     });
 }
 
 export async function getDailyLogs(log_date: string ): Promise<DailyLog[]> {
-    const res = await fetch(`/api/daily_logs?log_date=${log_date}`);
-    if (!res.ok) throw new Error("Failed to fetch logs");
-    return (await res.json()) as DailyLog[];
+    return request<DailyLog[]>(`/daily_logs?log_date=${log_date}`);
 }
