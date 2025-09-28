dc498ea8d7d6378d309a29e8b533d14b2a54a58f
CalvinGaiennie <calvingaiennie@gmail.com>
2025-09-28 16:59:06 -0500

    started building metric settings

--- src/components/SettingsEdit.tsx
+++ src/components/SettingsEdit.tsx
@@ -10,11 +10,28 @@ function SettingsEdit() {
   }, []);
 
   return (
-    <div>
+    <div className="card p-4 m-5 ">
       <h1>Settings</h1>
-      <p>
-        {settings?.homePageLayout.map((section) => section.section).join(", ")}
-      </p>
+      <div className="row">
+        {settings?.homePageLayout.map((section, index) => (
+          <div key={index} className="col-12 col-sm-6 col-md-6 col-lg-6 mb-3">
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">{section.section}</h5>
+                <div className="d-flex flex-column">
+                  {section.metricIds.map((metricId, metricIndex) => (
+                    <input
+                      key={metricIndex}
+                      className="mb-1 form-control"
+                      value={metricId}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
